cc257065514260a9550af0b751a97ca2527e31c9
Samuel-OR <samueloliveira0014@gmail.com>
2019-10-29 09:07:14 -0300

    add: função cadastrar professor e time

--- inicial.py
+++ inicial.py
@@ -12,14 +12,14 @@ import sys
 import os
 from PyQt5.QtCore import pyqtSlot
 
-
-import BancoDeDados as BD
+from sockets_cliente import *
 
 
 class Ui_Main(QtWidgets.QWidget):
     def setupUi(self, Main):
         Main.setObjectName('Main')
         Main.resize(640, 480)
+        self.client_socket = ClientSocket()
 
         self.QtStack = QtWidgets.QStackedLayout()
 
@@ -52,98 +52,68 @@ class Main(QMainWindow, Ui_Main):
         self.setupUi(self)
 
         self.tela_login.pushButton.clicked.connect(self.openTelaCadastrar)
-
-
         self.tela_login.pushButton_2.clicked.connect(self.entrar)
-
         self.tela_cadastrar.pushButton.clicked.connect(self.voltarLogin)
         #self.tela_cadastrar.pushButton_2.clicked.connect(self.voltarLogin)
-
         self.tela_Professor.pushButton_7.clicked.connect(self.voltarLogin)
-
+        self.tela_Professor.pushButton_2.clicked.connect(self.cadastrarTime)
         self.tela_team.pushButton_7.clicked.connect(self.voltarLogin)
-        '''self.tela_principal.ver_acervo.clicked.connect(self.openAcervoLivro)
-        self.tela_principal.sair.clicked.connect(self.voltarInicio)
 
-        self.tela_cadastro_livro.botao_salvar_livro.clicked.connect(self.cadastrarLivro)
-        self.tela_cadastro_livro.buttonVoltar.clicked.connect(self.voltarPrincipal)
+        self.tela_cadastrar.pushButton_2.clicked.connect(self.cadastrar)
 
-        self.tela_acervo.sair.clicked.connect(self.entrar)'''
 
     def openTelaCadastrar(self):
         self.QtStack.setCurrentIndex(1)
 
     def entrar(self):
-        '''
-        import socket
-        ip = 'localhost'
-        port = 7000
-        addr = ((ip,port)) #define a tupla de endereco
-        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #AF_INET parametro para informar a familia do protocolo, SOCK_STREAM indica que eh TCP/IP
-        client_socket.connect(addr) #realiza a conexao
+        string_login = "login,"
+
+        user = self.tela_login.lineEdit.text()
+        password = self.tela_login.lineEdit_2.text()
+        string_login+=user+","+password
+        #self.client_socket.enviar_dados(string_login)
+        self.QtStack.setCurrentIndex(2)
+
+
+
+    def cadastrar(self):
+        string_cadastro = "cadastro,"
+
+        email = self.tela_cadastrar.lineEdit.text()
+        password = self.tela_cadastrar.lineEdit_2.text()
+        name = self.tela_cadastrar.lineEdit_3.text()
+
+        string_cadastro+=name+","+email+","+password
+
+        print(string_cadastro)
+        if self.client_socket.enviar_dados(string_cadastro):
+            QMessageBox.about(None, "ATENÇÃO", "Cadastro Efetuado.") 
+            self.voltarLogin()
+        else:
+            QMessageBox.about(None, "ATENÇÃO", "Cadastro NÃO Efetuado.") 
+
+    def cadastrarTime(self):
+        string_cadastro_time = "cadastroTime,"
+
+        nameTeam = self.tela_Professor.lineEdit_2.text()
+        C1 = self.tela_Professor.lineEdit_9.text()
+        C2 = self.tela_Professor.lineEdit.text()
+        C3 = self.tela_Professor.lineEdit_10.text()
+        C4 = self.tela_Professor.lineEdit_8.text()
+
+        string_cadastro_time+=nameTeam+","+C1+","+C2+","+C3+","+C4
+        print(string_cadastro_time)
         
 
-        user = self.tela_login.lineEdit.text()
-        password = self.tela_login.lineEdit_2.text()
-        #cad = BD.login(user, password)
-        mensagem = str(user)+str(','+password)
-
-        #mensagem = input('digite uma mensagem para enviar ao servidor: ')
-        client_socket.send(mensagem.encode()) #envia mensagem
-
-        cad = client_socket.recv(1024).decode()
-        print('SHOW',cad)
-
-        if(cad[2] == 2):
-            BD.codTEACHE = user
-            self.QtStack.setCurrentIndex(2) #tela de login do professor
-        elif cad[2] == 1:
-            BD.codTeam = user
-            self.QtStack.setCurrentIndex(3) #tela de login do aluno
+        if self.client_socket.enviar_dados(string_cadastro_time):
+            QMessageBox.about(None, "ATENÇÃO", "Cadastro Efetuado.") 
+            self.voltarLogin()
         else:
-            QMessageBox.about(None, "ATENÇÃO", "Usúario ou Senha inválidos.") 
-            #self.show()
-
-        print ('mensagem enviada')
-       
-        client_socket.close() #fecha conexao
-        '''
-        #Sem Threads
-        user = self.tela_login.lineEdit.text()
-        password = self.tela_login.lineEdit_2.text()
-        cad = BD.login(user, password)
-        if(cad[2] == 2):
-            BD.codTEACHE = user
-            self.QtStack.setCurrentIndex(2) #tela de login do professor
-            #self.QtStack.setCurrentIndex(3) #tela de login do aluno
-        elif cad[2] == 1:
-            BD.codTeam = user
-            self.QtStack.setCurrentIndex(3) #tela de login do aluno
-        else:
-            QMessageBox.about(None, "ATENÇÃO", "Usúario ou Senha inválidos.") 
-            #self.show()
-
-        print ('mensagem enviada')
-       
-
+            QMessageBox.about(None, "ATENÇÃO", "Cadastro NÃO Efetuado.") 
         
     def voltarLogin(self):
         self.QtStack.setCurrentIndex(0)
 
-    '''def criarConta(self):
-        self.QtStack.setCurrentIndex(0)
-
-    def openCadastrarLivro(self):
-        self.QtStack.setCurrentIndex(3)
-
-    def openAcervoLivro(self):
-        self.QtStack.setCurrentIndex(4)
-
-    def voltarPrincipal(self):
-        self.QtStack.setCurrentIndex(2)
-
-    def cadastrarLivro(self):
-        self.QtStack.setCurrentIndex(2) '''
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
