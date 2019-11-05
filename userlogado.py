239179aea760f5c80171c00110113ca296318232
thalianegomes <thaliane.ramos123@gmail.com>
2019-11-05 03:21:01 -0300

    banco auxiliar

---
+++ userlogado.py
@@ -0,0 +1,17 @@
+class UserLogado():
+    def __init__(self):
+        self.id = str()
+        self.siape = str()
+        self.nome = str()
+        self.email = str()
+        self.senha = str()
+        self.times = int()
+
+class TimeBuscado():
+    def __init__(self):
+        self.id = str()
+        self.nome = str()
+        self.c1 = str()
+        self.c2 = str()
+        self.c3 = str()
+        self.c4 = str()
